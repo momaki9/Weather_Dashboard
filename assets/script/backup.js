0cb66dd36f61c4462e8c4ad384ee24d6853e734b
Mostafa Maki <mostafa_m9@yahoo.com>
2022-08-07 09:07:28 -0700

    added 5-day forecast

--- assets/script/backup.js
+++ assets/script/backup.js
@@ -72,3 +72,78 @@ function getWeatherApi() {
 
 getGeoCodeApi();
 getWeatherApi();
+
+
+// kind of works but it is 'behind' because of local storage
+var dateEl = $('#date');
+var tempDisplay = $('#temp')
+var windDisplay = $('#wind')
+var humididtyDisplay = $('#humidity')
+var uvindexDisplay = $('#uv-index')
+var input = $('.city-input')
+
+
+function dateToday() {
+    var currentTime = moment().format('(MM/DD/YYYY)');
+    dateEl.text(currentTime);
+};
+
+$('.srch-btn').on('click', function() {
+    var cityInput = input.val();
+    var cityUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&appid=f57f9a13f3296d2929f153a072066d23`;
+
+    getGeoCodeApi();
+
+    function getGeoCodeApi() {
+        var requestUrl = cityUrl;
+        fetch(requestUrl)
+        .then(function (response) {
+        return response.json();
+    })
+        .then(function (data) {
+        var cityLat = data[0].lat
+        localStorage.setItem("city-lat", cityLat.toFixed(2))
+        var cityLon = data[0].lon  
+        localStorage.setItem("city-lon", cityLon.toFixed(2))
+    });
+    }
+
+    var storedLat = localStorage.getItem("city-lat")
+    var storedLon = localStorage.getItem("city-lon")
+
+   
+
+    var weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${storedLat}&lon=${storedLon}&exclude=minutely,hourly,daily&units=imperial&appid=f57f9a13f3296d2929f153a072066d23`;
+
+    getWeatherApi();
+
+    function getWeatherApi() {
+        var requestUrl = weatherUrl;
+        fetch(requestUrl)
+            .then(function (response) {
+            return response.json();
+        })
+            .then(function (data) {
+                console.log(data)
+            var currentTemp = $('<span>')
+            currentTemp.text(`${data.current.temp}°F`)
+            tempDisplay.append(currentTemp)
+    
+            var currentWind = $('<span>')
+            currentWind.text(`${data.current.wind_speed} MPH`)
+            windDisplay.append(currentWind)
+    
+            var currentHumidity = $('<span>')
+            currentHumidity.text(`${data.current.humidity} %`)
+            humididtyDisplay.append(currentHumidity)
+    
+            var currentUvi = $('<span>')
+            currentUvi.text(data.current.uvi)
+            uvindexDisplay.append(currentUvi)
+        });
+    }
+
+
+})
+
+dateToday();
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,11 +1,21 @@
+var dateEl = $('#date');
+var cityEl = $('#city-name')
 var tempDisplay = $('#temp')
 var windDisplay = $('#wind')
 var humididtyDisplay = $('#humidity')
 var uvindexDisplay = $('#uv-index')
 var input = $('.city-input')
+var forecast = $('#forecast')
+
+
+function dateToday() {
+    var currentTime = moment().format('(MM/DD/YYYY)');
+    dateEl.text(currentTime);
+};
 
 $('.srch-btn').on('click', function() {
     var cityInput = input.val();
+    cityEl.text(cityInput);
     var cityUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&appid=f57f9a13f3296d2929f153a072066d23`;
 
     getGeoCodeApi();
@@ -18,50 +28,66 @@ $('.srch-btn').on('click', function() {
     })
         .then(function (data) {
         var cityLat = data[0].lat
-        localStorage.setItem("city-lat", cityLat.toFixed(2))
+        var storedLat = cityLat.toFixed(2)
         var cityLon = data[0].lon  
-        localStorage.setItem("city-lon", cityLon.toFixed(2))
+        var storedLon = cityLon.toFixed(2)
+
+        var weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${storedLat}&lon=${storedLon}&exclude=minutely,hourly&units=imperial&appid=f57f9a13f3296d2929f153a072066d23`;
+
+        getWeatherApi();
+    
+        function getWeatherApi() {
+            var requestUrl = weatherUrl;
+            fetch(requestUrl)
+                .then(function (response) {
+                return response.json();
+            })
+                .then(function (data) {
+                console.log(data)
+                var currentTemp = $('<span>')
+                currentTemp.text(`${data.current.temp}°F`)
+                tempDisplay.append(currentTemp)
+        
+                var currentWind = $('<span>')
+                currentWind.text(`${data.current.wind_speed} MPH`)
+                windDisplay.append(currentWind)
+        
+                var currentHumidity = $('<span>')
+                currentHumidity.text(`${data.current.humidity} %`)
+                humididtyDisplay.append(currentHumidity)
+        
+                var currentUvi = $('<span>')
+                currentUvi.text(data.current.uvi)
+                uvindexDisplay.append(currentUvi)
+
+                console.log(data.daily)
+
+                for (var i = 1; i < 6; i++) {
+                    var divSec = $('<div>')
+                    divSec.attr("class", "col-2 bg-dark bg-gradient custom")
+                    forecast.append(divSec)
+
+                    var dateEl = $('<h5>');
+                    dateEl.text('8/8/22')
+                    divSec.append(dateEl)
+
+                    var tempEl = $('<h6>')
+                    tempEl.text(`Temp: ${data.daily[i].temp.max}°F`)
+                    divSec.append(tempEl)
+
+                    var windEl = $('<h6>')
+                    windEl.text(`Wind: ${data.daily[i].wind_speed} MPH`)
+                    divSec.append(windEl)
+
+                    var humEl = $('<h6>')
+                    humEl.text(`Humidity: ${data.daily[i].humidity} %`)
+                    divSec.append(humEl)
+                  }
+            });
+        }
     });
     }
 
-    var storedLat = localStorage.getItem("city-lat")
-    var storedLon = localStorage.getItem("city-lon")
-
-    // console.log(storedLat.toFixed(2))
-
-    // var roundedStoredLat = storedLat.val().toFixed(2);
-
-    // var roundedStoredLon = storedLon.val().toFixed(2);
-
-    var weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${storedLat}&lon=${storedLon}&exclude=minutely,hourly,daily&appid=f57f9a13f3296d2929f153a072066d23`;
-
-    getWeatherApi();
-
-    function getWeatherApi() {
-        var requestUrl = weatherUrl;
-        fetch(requestUrl)
-            .then(function (response) {
-            return response.json();
-        })
-            .then(function (data) {
-            var currentTemp = $('<span>')
-            currentTemp.text(data.current.temp)
-            tempDisplay.append(currentTemp)
-    
-            var currentWind = $('<span>')
-            currentWind.text(data.current.wind_speed)
-            windDisplay.append(currentWind)
-    
-            var currentHumidity = $('<span>')
-            currentHumidity.text(data.current.humidity)
-            humididtyDisplay.append(currentHumidity)
-    
-            var currentUvi = $('<span>')
-            currentUvi.text(data.current.uvi)
-            uvindexDisplay.append(currentUvi)
-        });
-    }
-
-
 })
 
+dateToday();
